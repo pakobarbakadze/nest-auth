f692eea078b57a86574390b0570babebbe9d2d2e
pakobarbakadze <pakobarbakadze@gmail.com>
2023-11-16 15:55:56 +0400

    chore: env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 8000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  await app.listen(PORT);
 }
 bootstrap();
